1c1f6ecfe7f72250493fd257e4b48ae6f06dfb54
Karin Okawa <karin_kayon@icloud.com>
2025-12-11 08:42:58 +0900

    homeのviews.pyの中身を全て削除した

--- first_project/home/views.py
+++ first_project/home/views.py
@@ -1,21 +1,2 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.contrib import messages
-
-
-def login_view(request):
-    if request.method == 'POST':
-        email = request.POST['email']
-        password = request.POST['password']
-        user = authenticate(request, username=email, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')
-        else:
-            messages.error(request, 'メールアドレスまたはパスワードが違います。')
-    return render(request, 'home/login.html')
-
-def home(request):
-    return render(request, 'home/login.html')
 
     
